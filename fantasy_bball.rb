9bb5543bb48116430c5cb5e897a74e2f1ad96f2a
Nitin Shantharam <atomic.unit@gmail.com>
2014-11-07 10:41:02 -0800

    first commit

---
+++ fantasy_bball.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+COLUMNS = %w(No Player G Min Pts Reb Ast Stl Blk To Pf Dreb Oreb Fgm-a Pct 3gm-a Pct Ftm-a Pct eff)
+ALL_PLAYER_STATS = "http://www.hoopsstats.com/basketball/fantasy/nba/playerstats/15/1/eff/1-1"
+SG_STATS         = "http://www.hoopsstats.com/basketball/fantasy/nba/playerstats/15/3/eff/1-1"
+
+page = Nokogiri::HTML(open(ALL_PLAYER_STATS))
+
+stats = []
+page.css('table table.statscontent tr').each do |tr|
+  player_status = tr.css('td').collect(&:text)
+  stats << player_status.each_with_index.map{|stat, i| { COLUMNS[i] => stat }}.inject(&:merge)
+end
+
+puts stats.inspect
